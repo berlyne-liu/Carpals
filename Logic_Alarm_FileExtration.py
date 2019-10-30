aba1e7840809993f63922236746a80e54ce95a17
berlyne-liu <13750104197@139.com>
2019-10-30 17:56:37 +0800

    2019-10-29 21:18

--- Logic_Alarm_FileExtration.py
+++ Logic_Alarm_FileExtration.py
@@ -1,41 +1,70 @@
 # -*- coding: utf-8 -*-
 
 import csv
-
+import xlrd
 
 class Alarm_Extraction:
-    def __init__(self):
-        pass
-
     def textExtraction(self, filepath):
-        with open(filepath, 'r') as file:
-            file_list = file.readlines()
-            container = []
-            error = "扫描文件发现以下不符合规则的数据行（可忽略）：\n"
-            header = file_list[3].strip('\n').split('\t')
-            column_h = len(header)
-            for row in range(len(file_list)):
-                package = file_list[row].strip('\n').split('\t')
-                if len(package) == column_h:
-                    container.append(package)
-                else:
-                    error = error + ("第%s行的字段数为%s,不符合规则！\n" % (row, len(package)))
-        return container, error
+        container = []
+        error = "存在以下异常：\n"
+        try:
+            with open(filepath, 'r') as file:
+                file_list = file.readlines()
+                t_header = file_list[3].strip('\n').split('\t')
+                column_h = len(t_header)
+                for row in range(len(file_list)):
+                    package = file_list[row].strip('\n').split('\t')
+                    if row > 0 and len(package) == column_h:
+                        container.append(package)
+                    else:
+                        error = error + ("第%s行的字段数为%s,不符合规则！\n" % (row, len(package)))
+        except Exception as e:
+            error = error + str(e)
+            t_header = None
+        return t_header, container, error
 
     def csvExtraction(self, csvpath):
-        with open(csvpath, 'r', newline='') as file:
-            cr = csv.reader(file, delimiter="|")
-            container = []
-            for i, rows in enumerate(cr):
-                container.append(rows)
-        return container
+        container = []
+        error = "存在以下异常：\n"
+        try:
+            with open(csvpath, 'r') as file:
+                cr = csv.reader(file, delimiter="|", quotechar='"')
+                for i, rows in enumerate(cr):
+                    if i > 0:
+                        container.append(rows)
+                c_header = container[0]
+        except Exception as e:
+            error = error + str(e)
+            c_header = None
+        return c_header, container, error
+
+    def excelExtraction(self, excelpath):
+        container = []
+        error = "存在以下异常：\n"
+        try:
+            excelFile = xlrd.open_workbook(excelpath)
+            sheet_name = excelFile.sheet_names()
+            table = excelFile.sheet_by_name("标准告警码")
+            r = table.nrows
+            e_header = table.row_values(rowx=0, start_colx=0, end_colx=None)
+            for i in range(r):
+                if i > 0:
+                    container.append(table.row_values(rowx=i, start_colx=0, end_colx=None))
+        except Exception as e:
+            error = error + str(e)
+            e_header = None
+        return e_header, container, error
 
 
 if __name__ == "__main__":
     Path = "C:/Users/My-PC/Desktop/alarm/告警处理-工具/告警处理1022/FDD状态"
     Path2 = "C:/Users/My-PC/Desktop/alarm/告警处理-工具/告警处理1022/FDD告警.csv"
+    Path3 = "E:/Program Files/JetBrains/PyDemo/Github_Clone/告警设置.xlsx"
     Ae = Alarm_Extraction()
-    li, Error = Ae.textExtraction(Path)
-    print(li, Error)
-    ce = Ae.csvExtraction(Path2)
-    print(ce)
+    Ae.excelExtraction(Path3)
+    # header, cont, Error = Ae.textExtraction(Path)
+    # print(cont)
+    # ceh, cec, error2 = Ae.csvExtraction(Path2)
+    # print(cec)
+    h2, c2, e2 = Ae.excelExtraction(Path3)
+    print(c2)
---
+++ Logic_Sqlite_Modify.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+import sqlite3
+from Logic_Alarm_FileExtration import *
+
+
+class Sqlite_Modify:
+    def __init__(self, conn):
+        # 创建连接connect, 创建游标cur
+        self.connect = conn
+        self.cur = self.connect.cursor()
+
+    def sqlite_creat(self, *args, **kwargs):
+        """
+        *args:用于制作创建表的脚本的表头部分
+        **kwargs:用于识别需要创建的表名，格式table=xx
+        header是表头字符串，query按标准create table的格式拼接输出语句
+        """
+        table_head = ",".join("\"" + str(i) + "\"" for i in args[0])
+        query_c = """create table if not exists """ + kwargs['table'] + """ (""" + table_head + """)"""
+        # print(query_c)
+        self.cur.execute(query_c)
+
+    def sqlite_insert(self, i_head, *args, **kwargs):
+        """
+        *args:需导入的数据
+        **kwargs:用于识别需要导入的表名，格式table=xx
+        传入需导入的数据，传入类型为列表
+        """
+        # print(args)
+        str_head = ",".join("\"" + str(i) + "\"" for i in i_head)
+        for i, rows in enumerate(args[0]):
+            try:
+                str_sql = ",".join("\"" + str(i).replace("\"", "'") + "\"" for i in rows)
+                query_i = """insert into """ + kwargs[
+                    'table'] + """ (""" + str_head + """) values (""" + str_sql + """)"""
+                # print(query_i)
+                self.cur.execute(query_i)
+            except Exception as e:
+                print("第%s行出现异常：" % i + str(e) + "\n插入语句为：\n" + str(rows))
+        self.connect.commit()
+
+    def sqlite_output(self):
+        pass
+
+    def sqlite_query(self):
+        pass
+
+
+if __name__ == "__main__":
+    Path = "C:/Users/My-PC/Desktop/alarm/告警处理-工具/告警处理1022/FDD状态"
+    Path2 = "C:/Users/My-PC/Desktop/alarm/告警处理-工具/告警处理1022/FDD告警.csv"
+    Path3 = "E:/Program Files/JetBrains/PyDemo/Github_Clone/告警设置.xlsx"
+    connect = sqlite3.connect('Carpals.db')
+    modify = Sqlite_Modify(connect)
+    Ae = Alarm_Extraction()
+    # head, data, Error = Ae.textExtraction(Path)
+    # head1, data1, Error1 = Ae.csvExtraction(Path2)
+    h2, d2, e3 = Ae.excelExtraction(Path3)
+    modify.sqlite_creat(h2, table="Alarm_Standard")
+    modify.sqlite_insert(h2, d2, table="Alarm_Standard")
+
+    # modify.sqlite_creat(head1, table="Alarm_FDD_Cause")
+    # modify.sqlite_insert(head1, data1, table="Alarm_FDD_Cause")
+    #
+    # modify.sqlite_creat(head, table="Alarm_FDD_State")
+    # modify.sqlite_insert(head, data, table="Alarm_FDD_State")
